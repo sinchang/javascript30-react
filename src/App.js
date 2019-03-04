8dfcf21e1504aa8fd53a9af08ef4b789e481cb6a
sinchang <sinchangwen@gmail.com>
2019-03-04 11:35:54 +0800

    Finished CSS + JS Clock

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Day1 from './pages/day1/Day1'
+import Day2 from './pages/day2/Day2'
 class App extends Component {
   render() {
     return (
@@ -10,6 +11,7 @@ class App extends Component {
         <div className="App">
           <Route exact path="/" component={Home} />
           <Route exact path="/day1" component={Day1} />
+          <Route exact path="/day2" component={Day2} />
         </div>
       </Router>
     )
