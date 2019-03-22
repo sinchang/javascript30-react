8e27505b5415ba17c1982055a8633abd467f023a
Jeff Wen <sinchangwen@gmail.com>
2019-03-22 22:47:44 +0800

    Complete Custom HTML5 Video Player (#7)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Variables from './pages/day3/Variables'
 import Panels from './pages/day5/Panels'
 import Type from './pages/day6/Type'
 import Checkbox from './pages/day10/Checkbox'
+import Player from './pages/day11/Player'
 class App extends Component {
   render() {
     return (
@@ -20,6 +21,7 @@ class App extends Component {
           <Route exact path="/day5" component={Panels} />
           <Route exact path="/day6" component={Type} />
           <Route exact path="/day10" component={Checkbox} />
+          <Route exact path="/day11" component={Player} />
         </div>
       </Router>
     )
